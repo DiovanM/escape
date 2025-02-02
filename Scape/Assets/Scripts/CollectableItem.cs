290be1f60013b8f38d238576f6e9f9966040c16c
Diovan Marques <diovanmarques@gmail.com>
2025-02-02 11:07:14 -0300

    Unity 6 update

--- Scape/Assets/Scripts/CollectableItem.cs
+++ Scape/Assets/Scripts/CollectableItem.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CollectableItem : MonoBehaviour
 {
     [SerializeField] private new Renderer renderer;
     public ItemSO itemSO;
 
+    private void Awake()
+    {
+        if (renderer == null) GetComponent<Renderer>();
+    }
+
     public void Collect()
     {
-        renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        renderer.shadowCastingMode = ShadowCastingMode.Off;
+        renderer.receiveShadows = false;
         Debug.Log("Collected " + itemSO.itemKey, gameObject);
     }
 
     public void Use()
     {
-        renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        renderer.shadowCastingMode = ShadowCastingMode.On;
+        renderer.receiveShadows = true;
         Debug.Log("Item Used " + itemSO.itemKey);
     }
 
